ca646d8cd339c3e6c09945ec3805308ba6cf432b
Ivar Johansson <ivar@webbmaffian.se>
2022-03-29 16:27:50 +0200

    Updated readme.txt

---
+++ trunk/readme.txt
@@ -0,0 +1,19 @@
+=== Zaver Checkout for WooCommerce ===
+Tags: woocommerce, zaver, checkout, payment, sdk, credit card, refund, swish
+Stable tag: 1.0.0
+Requires at least: 4.7
+Tested up to: 5.9.2
+Requires PHP: 7.4
+License: GNU General Public License v3.0
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+WooCommerce plugin for Zaver Checkout.
+
+== Description ==
+
+WooCommerce plugin for Zaver Checkout.
+
+== Changelog ==
+
+= 1.0.0 =
+* Added Zaver Checkout and support for refunds.
--- trunk/woocommerce-zaver-checkout.php
+++ trunk/woocommerce-zaver-checkout.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Zaver Checkout
  * Plugin URI: 
  * Description: The official Zaver Checkout payment gateway for WooCommerce.
- * Version: 1.0.0-rc1
+ * Version: 1.0.0
  * Author: Zaver
  * Author URI: https://www.zaver.io/
  * Developer: Webbmaffian
@@ -15,7 +15,7 @@
  * WC tested up to: 6.2.1
  *
  * License: GNU General Public License v3.0
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
 
 namespace Zaver;
