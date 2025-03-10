989a15bf6e016a9462f491a8a5b54738c057bb7e
Montazar <github.v0its@slmail.me>
2025-03-10 10:44:54 +0100

    Adds Zaver payment method icons

--- templates/payment-categories.php
+++ templates/payment-categories.php
@@ -18,15 +18,69 @@ $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
 $gateway            = $available_gateways[ Plugin::PAYMENT_METHOD ];
 $chosen_gateway     = $available_gateways[ array_key_first( $available_gateways ) ];
 
+$mapping = array(
+	'BANK_TRANSFER'   => array(
+		'id'          => 'bank_transfer',
+		'icon_id'     => 'BankTransfer.svg',
+		'pretty_name' => 'Bank Transfer',
+	),
+	'INSTALLMENTS'    => array(
+		'id'          => 'installments',
+		'icon_id'     => 'Installments.svg',
+		'pretty_name' => 'Installments',
+	),
+	'INVOICE'         => array(
+		'id'          => 'invoice',
+		'icon_id'     => 'Invoice.svg',
+		'pretty_name' => 'Invoice',
+	),
+	'MONTHLY_INVOICE' => array(
+		'id'          => 'monthly_invoice',
+		'icon_id'     => 'MonthlyInvoice.svg',
+		'pretty_name' => 'Monthly Invoice',
+	),
+	'PAY_NOW'         => array(
+		'id'          => 'pay_now',
+		'icon_id'     => 'PayNow.svg',
+		'pretty_name' => 'Pay Now',
+	),
+	'SWISH'           => array(
+		'id'          => 'swish',
+		'icon_id'     => 'Swish.svg',
+		'pretty_name' => 'Swish',
+	),
+	'VIPPS'           => array(
+		'id'          => 'vipps',
+		'icon_id'     => 'Vipps.svg',
+		'pretty_name' => 'Vipps',
+	),
+);
+
 foreach ( $payment_methods as $payment_method ) {
 	$checkout_token = $payment_method->getCheckoutToken();
 	$payment_link   = $payment_method->getPaymentLink();
-	$gateway_id     = Plugin::PAYMENT_METHOD . '_' . strtolower( $payment_method->getPaymentMethod() );
-	$pretty_name    = ucwords( str_replace( '_', ' ', strtolower( $payment_method->getPaymentMethod() ) ) );
+
+	$is_mapped = isset( $mapping[ $payment_method->getPaymentMethod() ] );
+	if ( $is_mapped ) {
+		$mapped_method = $mapping[ $payment_method->getPaymentMethod() ];
+		$gateway_id    = Plugin::PAYMENT_METHOD . "_{$mapped_method['id']}";
+		$pretty_name   = $mapped_method['pretty_name'];
+	} else {
+		$gateway_id  = Plugin::PAYMENT_METHOD . '_' . strtolower( $payment_method->getPaymentMethod() );
+		$pretty_name = ucwords( str_replace( '_', ' ', strtolower( $payment_method->getPaymentMethod() ) ) );
+	}
 
 	$gateway        = $available_gateways[ Plugin::PAYMENT_METHOD ] ?? $available_gateways[ $gateway_id ];
 	$gateway->id    = $gateway_id;
 	$gateway->title = $pretty_name;
+
+	$default_icon_url = plugin_dir_url( ZCO_MAIN_FILE ) . 'assets/icon.svg';
+	$icon_path_url    = $default_icon_url;
+	if ( $is_mapped ) {
+		$icon_type     = 'Icon-Plate'; // 'Icon' or 'Icon-Plate'.
+		$icon_path_url = plugin_dir_url( ZCO_MAIN_FILE ) . "assets/payment-methods/{$icon_type}-{$mapped_method['icon_id']}";
+	}
+	$gateway->icon = $icon_path_url;
 
 	$session[ $gateway_id ] = array(
 		'token' => $checkout_token,
@@ -37,7 +91,7 @@ foreach ( $payment_methods as $payment_method ) {
 	if ( false !== strpos( $chosen_gateway->id, Plugin::PAYMENT_METHOD ) || $gateway->chosen ) {
 		$gateway->chosen = false;
 		// ... set the first payment method received from Zaver as chosen.
-		if ( $payment_method->getPaymentMethod() === $payment_methods[ array_key_first( $payment_methods ) ]->getPaymentMethod() ) {
+		if ( $payment_method === $payment_methods[ array_key_first( $payment_methods ) ]->getPaymentMethod() ) {
 			$gateway->chosen = true;
 		}
 	}
