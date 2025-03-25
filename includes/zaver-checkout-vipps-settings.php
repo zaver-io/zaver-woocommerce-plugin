6b5c484a1c6c59de0ed0c668d020063abc08b75b
Montazar <github.v0its@slmail.me>
2025-03-25 16:50:29 +0100

    Removes primary color settings
    Only relevant for embedded which is removed.

--- includes/zaver-checkout-vipps-settings.php
+++ includes/zaver-checkout-vipps-settings.php
@@ -15,25 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 return apply_filters(
 	'zaver_checkout_vipps_settings',
 	array(
-		'enabled'       => array(
+		'enabled' => array(
 			'type'    => 'checkbox',
 			'default' => 'yes',
 			'title'   => __( 'Enable/Disable', 'zco' ),
 			'label'   => __( 'Enable Zaver Checkout', 'zco' ),
 		),
-		'title'         => array(
+		'title'   => array(
 			'type'        => 'text',
 			'desc_tip'    => true,
 			'title'       => __( 'Title', 'zco' ),
 			'description' => __( 'This controls the title which the user sees during checkout.', 'zco' ),
 			'default'     => __( 'Vipps', 'zco' ),
-		),
-		'primary_color' => array(
-			'type'        => 'color',
-			'desc_tip'    => true,
-			'title'       => __( 'Primary color', 'zco' ),
-			'description' => __( 'Some elements in the Zaver Checkout will get this color.', 'zco' ),
-			'placeholder' => __( 'Default', 'zco' ),
 		),
 	)
 );
