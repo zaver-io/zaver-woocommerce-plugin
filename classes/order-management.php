98da89dfb9bb47be1ef4f3145c89811fced17926
Montazar <github.v0its@slmail.me>
2025-05-13 15:24:12 +0200

    Fixes format_price

--- classes/order-management.php
+++ classes/order-management.php
@@ -223,7 +223,7 @@ class Order_Management {
 	 * @return string The formatted price.
 	 */
 	public static function format_price( $amount, $currency ) {
-		return substr_replace( $amount, wc_get_price_decimal_separator(), -2, 0 ) . ' ' . $currency;
+		return preg_replace( '/[.,]/', wc_get_price_decimal_separator(), $amount, 1 ) . ' ' . $currency;
 	}
 }
 
